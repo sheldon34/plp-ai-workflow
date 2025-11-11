b47d1248872aec8bd5f28945121788b470cc1d96
sheldon34 <mubokasheldonmuboka@gmail.com>
2025-11-11 21:35:52 +0300

    model training

--- DataCollection.py
+++ DataCollection.py
@@ -56,3 +56,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # Select the logistic regression model
 model = LogisticRegression()
+
+# Train the model
+model.fit(X_train, y_train)
